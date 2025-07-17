1046d95a5d35a6bc264b840726dc36ab31a57742
nattibp <nattiberry12@gmail.com>
2025-07-17 16:47:23 +0700

    com2

--- menu.php
+++ menu.php
@@ -1,11 +1,5 @@
 <?php
     echo" Home";
     echo" Product";
-<<<<<<< HEAD
-    echo "Mahanakorn";
-=======
-    echo" map";
-    echo" About me111";
-
->>>>>>> d6280a1e1a764fce5780c9affd8d0f0ebeeca38f
+    echo "Mahanakorn111";
 ?>
